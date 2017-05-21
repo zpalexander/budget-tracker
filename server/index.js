54b17779902726202477eec5f4882e56b2741753
Zach Alexander <zpa@amazon.com>
2017-05-20 18:50:51 -0700

    Allow webpack dev server to proxy requests to express

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
 const app = require('./app');
+
+const PROTOCOL = process.env.HTTPS === 'true' ? 'https' : 'http';
+const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 9000;
-app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+
+app.listen(PORT, () => console.log(`App listening at ${PROTOCOL}://${HOST}:${PORT}`));
