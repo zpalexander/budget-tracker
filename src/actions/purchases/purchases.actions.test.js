de08c18345fa97d8da996d7a3eec77fff388b416
Zach Alexander <zpa@amazon.com>
2017-10-07 11:44:37 -0700

    Reqruite purchase action tests for ava

--- src/actions/purchases/purchases.actions.test.js
+++ src/actions/purchases/purchases.actions.test.js
@@ -1,54 +1,53 @@
 import test from 'ava';
 
-console.log('test file hit');
+import {
+    SET_PURCHASES,
+    setPurchases,
+    GET_PURCHASES,
+    getPurchases,
+    SAVE_NEW_PURCHASE,
+    saveNewPurchase,
+    DELETE_PURCHASE,
+    deletePurchase
+} from './purchases.actions.js';
 
-test('my passing test', t => {
-    t.pass();
+test('sets the list of purchases', t => {
+    const mockPurchases = [0, 1, 2];
+    t.deepEqual(setPurchases(mockPurchases), {
+        type: SET_PURCHASES,
+        purchases: mockPurchases,
+    });
 });
 
-// import {
-//     SET_PURCHASES,
-//     setPurchases,
-//     GET_PURCHASES,
-//     getPurchases,
-//     SAVE_NEW_PURCHASE,
-//     saveNewPurchase,
-//     DELETE_PURCHASE,
-//     deletePurchase
-// } from './purchases.actions.js';
+test('saves a new purchase', t => {
+    const mockName = 'name';
+    const mockAmount = 100;
+    t.deepEqual(saveNewPurchase(mockName, mockAmount), {
+        type: SAVE_NEW_PURCHASE,
+        name: mockName,
+        amount: mockAmount,
+    });
+});
 
-// it('sets the list of purchases', () => {
-//     const mockPurchases = [0, 1, 2];
-//     expect(setPurchases(mockPurchases)).toEqual({
-//         type: SET_PURCHASES,
-//         purchases: mockPurchases,
-//     });
-// });
+test('rounds a new purchase amount to two decimal places', t => {
+    const mockName = 'name';
+    const mockAmount = 100.987;
+    t.deepEqual(saveNewPurchase(mockName, mockAmount), {
+        type: SAVE_NEW_PURCHASE,
+        name: mockName,
+        amount: 100.99,
+    });
+});
 
-// it('saves a new purchase', () => {
-//     const mockName = 'name';
-//     const mockAmount = 100;
-//     expect(saveNewPurchase(mockName, mockAmount)).toEqual({
-//         type: SAVE_NEW_PURCHASE,
-//         name: mockName,
-//         amount: mockAmount,
-//     });
-// });
+test('deletes a purchase with a specified id', t => {
+    const mockId = 1;
+    t.deepEqual(deletePurchase(mockId), {
+        type: DELETE_PURCHASE,
+        id: mockId,
+    });
+});
 
-// it('rounds a new purchase amount to two decimal places', () => {
-//     const mockName = 'name';
-//     const mockAmount = 100.987;
-//     expect(saveNewPurchase(mockName, mockAmount)).toEqual({
-//         type: SAVE_NEW_PURCHASE,
-//         name: mockName,
-//         amount: 100.99,
-//     });
-// });
 
 // it('deletes a purchase with a specified id', () => {
-//     const mockId = 1;
-//     expect(deletePurchase(mockId)).toEqual({
-//         type: DELETE_PURCHASE,
-//         id: mockId,
-//     })
+
 // });
