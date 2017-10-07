315475639ed3af941c65124621d2ca7277405a2b
Zach Alexander <zpa@amazon.com>
2017-10-07 12:36:54 -0700

    Tests for ui actions

--- src/actions/ui/ui.actions.test.js
+++ src/actions/ui/ui.actions.test.js
@@ -1,5 +1,16 @@
 import test from 'ava';
 
-test('passing test', t => {
-    t.pass();
+import {
+    TOGGLE_NAV,
+    toggleNav,
+    TOGGLE_ADD_NEW_PURCHASE_DIALOG,
+    toggleAddNewPurchaseDialog
+} from './ui.actions';
+
+test('should create an action to toggle the nav', t => {
+    t.deepEqual(toggleNav(), { type: TOGGLE_NAV })
+});
+
+test('should create an action to toggle the new purchase dialog', t => {
+    t.deepEqual(toggleAddNewPurchaseDialog(), { type: TOGGLE_ADD_NEW_PURCHASE_DIALOG });
 });
